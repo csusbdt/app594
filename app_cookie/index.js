206bd16ce6773867377426508b1baf9a7ab1e386
turner <csusbdt@gmail.com>
2012-12-29 13:11:34 -0800

    debugging

--- app_cookie/index.js
+++ app_cookie/index.js
@@ -12,6 +12,9 @@ exports.cookieDelete = appCookieName + '=deleted; Expires=Thu, 01-Jan-1970 00:00
 exports.extract = function(req) {
   var userCredentials;
   if (req.headers.cookie === undefined) return undefined;
+  console.log('-------------------------------------------------------------------------');
+  console.log(req.headers.cookie);
+  console.log(JSON.stringify(req.headers.cookie));
   var startIndex = req.headers.cookie.indexOf(appCookieName);
   if (startIndex === -1) return undefined;
   startIndex += appCookieName.length + 1;
@@ -19,10 +22,13 @@ exports.extract = function(req) {
   var onePastEndIndex = req.headers.cookie.indexOf(';', startIndex);
   if (onePastEndIndex === -1) onePastEndIndex = req.headers.cookie.length;
   try {
+    console.log(req.headers.cookie.substring(startIndex, onePastEndIndex));
     userCredentials = JSON.parse(req.headers.cookie.substring(startIndex, onePastEndIndex));
   } catch (e) {
     console.log('Bad app cookie.');
+    console.log('-------------------------------------------------------------------------');
     return undefined;
   }
+  console.log('-------------------------------------------------------------------------');
   return userCredentials;
 }
