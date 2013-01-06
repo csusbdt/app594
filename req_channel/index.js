b155590245100399898050b8750ddad1d075368f
turner <csusbdt@gmail.com>
2013-01-05 23:57:10 -0800

    fixed bug

--- req_channel/index.js
+++ req_channel/index.js
@@ -6,7 +6,7 @@ exports.handle = function(req, res) {
     'Content-Length': channelDoc.length,
     'Pragma': 'public',
     'Cache-Control': 'max-age=31536000',
-    'Expires': new Date(Date.now() + 31536000).toUTCString()
+    'Expires': new Date(Date.now() + 31536000000).toUTCString()
   });
   res.end(channelDoc);
 }
